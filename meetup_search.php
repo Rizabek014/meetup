3da2aa271cedda0b0b43ad70ddc892d9856fe532
Rizabek014 <rizabekiitu@gmail.com>
2019-05-23 01:11:26 +0600

    revert meetup search

--- meetup_search.php
+++ meetup_search.php
@@ -1,22 +1,17 @@
 <?php
-    $GLOBALS['filename'] = 'meetup_search';
-    
-    include ('Database.php');
-    include ('nav.php');
+$GLOBALS['filename'] = 'meetup_search';
 
-
-    $result = NULL;
-
-    if(isset($_POST['find']))
-    {
-        $search = $_POST['search_field'];
-        $sql = "SELECT * FROM meetups WHERE name LIKE '%". $search . "%' OR location LIKE '%". $search . "%'";
-        $result = mysqli_query($db,$sql);
-        $results = mysqli_fetch_array($result);
-    }
-
-    $meetup = mysqli_query($db, "SELECT * FROM meetups");
-    $image = mysqli_query($db, "SELECT * FROM image");
+include ('Database.php');
+include ('nav.php');
+$result = NULL;
+if(isset($_POST['find']))
+{
+    $search = $_POST['search_field'];
+    $sql = "SELECT * FROM meetups WHERE name LIKE '%". $search . "%' OR location LIKE '%". $search . "%'";
+    $result = mysqli_query($db,$sql);
+}
+$meetup = mysqli_query($db, "SELECT * FROM meetups");
+$image = mysqli_query($db, "SELECT * FROM image");
 
 ?>
 <main id="main" class="main-page">
@@ -28,36 +23,35 @@
             </div>
             <div class="row">
                 <?php
-                     while ($tuple = mysqli_fetch_array($result)):
+                if(isset($result)):
+                    while ($tuple = mysqli_fetch_array($result)):
                         if($tuple['is_approved'] == 1):
-                ?>
-                <div class="col-lg-4 col-md-6">
-                    <div class="speaker">
-                        <?php
-                        $check = true;
-                        foreach ($image as $images) {
-                            if ($images['meetup_id'] == $tuple['meetup_id'] && $check)
-                            {
-                                $check = false;
-                                echo "<img class='img-fluid' src='images/" . $images['file_name'] . "'>";
-                            }
-                        }
-                        ?>
-                        <div class="details">
-                            <h3 class='clickable-row' data-href="meetup_details.php?meetup=<?php echo $tuple['meetup_id']; ?>"><?php echo $tuple['name']; ?></h3>
-                            <p><?php echo $tuple['location']; ?></p>
-                            <div class="social">
-                                <a href=""><i class="fa fa-twitter"></i></a>
-                                <a href=""><i class="fa fa-facebook"></i></a>
-                                <a href=""><i class="fa fa-google-plus"></i></a>
-                                <a href=""><i class="fa fa-linkedin"></i></a>
+                            ?>
+                            <div class="col-lg-4 col-md-6">
+                                <div class="speaker">
+                                    <?php
+                                    $check = true;
+                                    foreach ($image as $images) {
+                                        if ($images['meetup_id'] == $tuple['meetup_id'] && $check)
+                                        {
+                                            $check = false;
+                                            echo "<img class='img-fluid' src='images/" . $images['file_name'] . "'>";
+                                        }
+                                    }
+                                    ?>
+                                    <div class="details">
+                                        <h3 class='clickable-row' data-href="meetup_details.php?meetup=<?php echo $tuple['meetup_id']; ?>"><?php echo $tuple['name']; ?></h3>
+                                        <p><?php echo $tuple['location']; ?></p>
+                                        <div class="social">
+                                            <a href=""><i class="fa fa-twitter"></i></a>
+                                            <a href=""><i class="fa fa-facebook"></i></a>
+                                            <a href=""><i class="fa fa-google-plus"></i></a>
+                                            <a href=""><i class="fa fa-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <?php endif; endwhile; if(is_null($results)):?>
-                    <p>There is no search result</p>
-                <?php endif;?>
+                        <?php endif; endwhile; endif; ?>
             </div>
         </div>
     </section>
@@ -65,10 +59,3 @@
 <?php include ('footer.php'); ?>
 </body>
 </html>
-<script>
-    jQuery(document).ready(function($) {
-        $(".clickable-row").click(function() {
-            window.location = $(this).data("href");
-        });
-    });
-</script>
