5cae4895dde0e2e52ef4be2140ed8b249e0643f7
Rizabek014 <rizabekiitu@gmail.com>
2019-05-23 22:35:58 +0600

    dividing meetup list, need to optimization and designing

--- meetup_list.php
+++ meetup_list.php
@@ -9,6 +9,7 @@
         $user_id = $_GET['user_id'];
         $sql = "SELECT * FROM meetups WHERE organizer_id = '$user_id'";
         $result = mysqli_query($db,$sql);
+        $results = mysqli_query($db,$sql);
     }
     $image = mysqli_query($db, "SELECT * FROM image");
 
@@ -19,12 +20,13 @@
             <div class="section-header">
                 <h2>My Meetups</h2>
             </div>
+            <h2>Approved</h2>
             <div class="row">
                 <?php
                     while ($tuple = mysqli_fetch_array($result)):
+                        if($tuple['is_approved']):
                 ?>
                     <div class="col-lg-4 col-md-6">
-                       <?php if($tuple['is_approved']) echo 'Approved'; else echo 'Waiting for approvement'?>
                         <div class="speaker">
                             <?php
                                 $check = true;
@@ -49,7 +51,43 @@
                         </div>
                     </div>
                     <?php
+                    endif;
                     endwhile;
+                ?>
+            </div>
+            <h2>Waiting for approvement</h2>
+            <div class="row">
+                <?php
+                while ($tuple = mysqli_fetch_array($results)):
+                    if(!$tuple['is_approved']):
+                        ?>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="speaker">
+                                <?php
+                                $check = true;
+                                foreach ($image as $images) {
+                                    if ($images['meetup_id'] == $tuple['meetup_id'] && $check)
+                                    {
+                                        $check = false;
+                                        echo "<img class='img-fluid' src='images/" . $images['file_name'] . "'>";
+                                    }
+                                }
+                                ?>
+                                <div class="details">
+                                    <h3 class='clickable-row' data-href="meetup_details.php?meetup=<?php echo $tuple['meetup_id']; ?>"><?php echo $tuple['name']; ?></h3>
+                                    <p><?php echo $tuple['location']; ?></p>
+                                    <div class="social">
+                                        <a href=""><i class="fa fa-twitter"></i></a>
+                                        <a href=""><i class="fa fa-facebook"></i></a>
+                                        <a href=""><i class="fa fa-google-plus"></i></a>
+                                        <a href=""><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    endif;
+                endwhile;
                 ?>
             </div>
         </div>
